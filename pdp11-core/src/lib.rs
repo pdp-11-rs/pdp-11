f2c2142571a3c83877feab0f8ad8497f3395694c
Cyril Plisko <cyril.plisko@mountall.com>
2026-01-29 14:17:45 +0200

    Convert to workspace
    Closes pdp-11-lx8

---
+++ pdp11-core/src/lib.rs
@@ -0,0 +1,10 @@
+use std::fmt;
+use std::fs;
+use std::io;
+use std::ops;
+use std::path::Path;
+use std::path::PathBuf;
+
+pub use cpu::Cpu;
+
+pub mod cpu;
--- pdp11/src/main.rs
+++ pdp11/src/main.rs
@@ -1,10 +1,6 @@
-use std::fmt;
-use std::fs;
 use std::io;
-use std::ops;
-use std::path::{Path, PathBuf};
 
-mod cpu;
+use pdp11_core::cpu;
 
 fn main() -> io::Result<()> {
     let core = cpu::Cpu::new("rk0.img")?;
