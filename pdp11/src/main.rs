803ed4949e3a4dbf728f7cb0bab272012f63023b
Cyril Plisko <cyril.plisko@mountall.com>
2026-01-30 23:04:31 +0200

    Add tracing initialization in main
    - Add tracing and tracing-subscriber dependencies to pdp11 binary
    - Initialize tracing subscriber with env-filter support
    - Default to INFO level, configurable via RUST_LOG env var
    - Enables runtime control of tracing output levels

--- pdp11/src/main.rs
+++ pdp11/src/main.rs
@@ -3,6 +3,14 @@ use std::io;
 use pdp11_core::cpu;
 
 fn main() -> io::Result<()> {
+    // Initialize tracing subscriber
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::from_default_env()
+                .add_directive(tracing::Level::INFO.into()),
+        )
+        .init();
+
     let core = cpu::Cpu::new("rk0.img")?;
     core.poweron();
     Ok(())
